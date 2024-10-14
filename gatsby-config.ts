e2e05d9ea6c61453b41d2b708fabcfc8f547bba6
MINWOO LEE <devminwoolee@gmail.com>
2024-10-14 07:46:27 +0000

    chore: add gatsby-source-filesystem package and configuration

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -9,7 +9,16 @@ const config: GatsbyConfig = {
   // If you use VSCode you can also use the GraphQL plugin
   // Learn more at: https://gatsby.dev/graphql-typegen
   graphqlTypegen: true,
-  plugins: ["gatsby-plugin-postcss"]
+  plugins: [
+    "gatsby-plugin-postcss",
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `pages`,
+        path: `${__dirname}/static/pages/`,
+      },
+    }
+  ]
 };
 
 export default config;
