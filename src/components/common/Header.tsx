7aa8b3b3f72b811a0714e1e0c62376f3300ac3df
MINWOO LEE <devminwoolee@gmail.com>
2024-10-16 05:21:14 +0000

    refactor: Remove unnecessary code

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -1,16 +1,15 @@
-// import * as React from "react"
 import React from "react"
-import type { HeadFC, PageProps } from "gatsby"
 import { Link } from "gatsby"
 
 const Header: React.FC = () => {
   return (
     <header className="flex h-16 border-b">
         <div className="ml-24 my-auto">
-            <Link to="/">
+            <Link to="/" className="flex">
                 <h1 className="text-gray-950 text-xl font-bold">
                     @Kimssammwu
                 </h1>
+                <p className="text-gray-700 text-xl">&nbsp;Blog</p>
             </Link>
         </div>
     </header>
