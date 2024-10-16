bd6a5735eff8833bb003cdd36bf10fce92eaf1f3
MINWOO LEE <devminwoolee@gmail.com>
2024-10-16 05:32:52 +0000

    feat: add  text to header main logo

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -6,9 +6,7 @@ const Header: React.FC = () => {
     <header className="flex h-16 border-b">
         <div className="ml-24 my-auto">
             <Link to="/" className="flex">
-                <h1 className="text-gray-950 text-xl font-bold">
-                    @Kimssammwu
-                </h1>
+                <h1 className="text-gray-950 text-xl font-bold">@Kimssammwu</h1>
                 <p className="text-gray-700 text-xl">&nbsp;Blog</p>
             </Link>
         </div>
