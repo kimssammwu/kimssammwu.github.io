72331ef976cddd35df5b427a6f39066f391bcd5f
MINWOO LEE <devminwoolee@gmail.com>
2024-10-17 11:10:19 +0000

    feat: support LaTex syntax using katex lib.

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -29,7 +29,6 @@ const config: GatsbyConfig = {
         ],
       } 
     },
-    // `gatsby-transformer-remark`,
     {
       resolve: `gatsby-transformer-remark`,
       options: {
@@ -43,6 +42,13 @@ const config: GatsbyConfig = {
               lineNumbers: true,
             },
           },
+          {
+            resolve: `gatsby-remark-katex`,
+            options: {
+              // Add any KaTeX options from https://github.com/KaTeX/KaTeX/blob/master/docs/options.md here
+              strict: `ignore`
+            }
+          }
         ],
       },
     },  
