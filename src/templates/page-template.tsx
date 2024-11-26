795a07407c2d59e56ded8bb2d92e1e056a433080
MINWOO LEE <devminwoolee@gmail.com>
2024-11-26 12:26:34 +0000

    feat: move FormatDate function to utils

--- src/templates/page-template.tsx
+++ src/templates/page-template.tsx
@@ -22,20 +22,6 @@ function expectedReadTime(content: string) {
   return Math.ceil(allWords / WordPerMinute); 
 } 
 
-function formatDate(dateString: string, locale: string = 'ko-KR', options: Intl.DateTimeFormatOptions = {}): string {
-  const date = new Date(dateString);
-  
-  const defaultOptions: Intl.DateTimeFormatOptions = {
-    year: 'numeric',
-    month: 'long',
-    day: 'numeric',
-    weekday: 'long',
-    ...options,
-  };
-
-  const formatter = new Intl.DateTimeFormat(locale, defaultOptions);
-  return formatter.format(date);
-}
 
 
 export const query = graphql`
