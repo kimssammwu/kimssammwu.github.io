005dce1a88b8bdaa9c4a5b29054a4e8298defac7
MINWOO LEE <devminwoolee@gmail.com>
2024-11-27 14:05:19 +0000

    feat: add SEO object

---
+++ src/components/common/seo.tsx
@@ -0,0 +1,48 @@
+import React from "react"
+import Helmet from "react-helmet"
+import { useSiteMetadata } from "../hooks/use-site-metadata"
+
+interface SEOArguments {
+	title?: string;
+	description?: string;
+	pathname?: string;
+}
+
+export const SEOForTemplate = ({title, description, pathname}: SEOArguments) => {
+	const { title: defaultTitle, description: defaultDescription, image, siteUrl } = useSiteMetadata()
+
+	const seo = {
+		title: title || defaultTitle,
+		description: description || defaultDescription,
+		image: `${siteUrl}${image}`,
+		url: `${siteUrl}${pathname || ``}`,
+	}
+
+	return (
+		<Helmet>
+			<title>{seo.title}</title>
+			
+			<link rel="apple-touch-icon" sizes="57x57" href="/favicons/apple-icon-57x57.png" />
+			<link rel="apple-touch-icon" sizes="60x60" href="/favicons/apple-icon-60x60.png" />
+			<link rel="apple-touch-icon" sizes="72x72" href="/favicons/apple-icon-72x72.png" />
+			<link rel="apple-touch-icon" sizes="76x76" href="/favicons/apple-icon-76x76.png" />
+			<link rel="apple-touch-icon" sizes="114x114" href="/favicons/apple-icon-114x114.png" />
+			<link rel="apple-touch-icon" sizes="120x120" href="/favicons/apple-icon-120x120.png" />
+			<link rel="apple-touch-icon" sizes="144x144" href="/favicons/apple-icon-144x144.png" />
+			<link rel="apple-touch-icon" sizes="152x152" href="/favicons/apple-icon-152x152.png" />
+			<link rel="apple-touch-icon" sizes="180x180" href="/favicons/apple-icon-180x180.png" />
+			<link rel="icon" type="image/png" sizes="192x192"  href="/favicons/android-icon-192x192.png" />
+			<link rel="icon" type="image/png" sizes="32x32" href="/favicons/favicon-32x32.png" />
+			<link rel="icon" type="image/png" sizes="96x96" href="/favicons/favicon-96x96.png" />
+			<link rel="icon" type="image/png" sizes="16x16" href="/favicons/favicon-16x16.png" />
+			{/* TODO: OpenGraph, Twitter Image 각 게시글에 맞게 변경 필요 */}
+			<meta name="description" content={seo.description} />
+			<meta name="image" content={seo.image} />
+			<meta name="twitter:card" content="summary_large_image" />
+			<meta name="twitter:title" content={seo.title} />
+			<meta name="twitter:url" content={seo.url} />
+			<meta name="twitter:description" content={seo.description} />
+			<meta name="twitter:image" content={seo.image} />
+		</Helmet>
+	)
+}
