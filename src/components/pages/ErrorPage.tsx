349d6f9c9fac3205a59be2f9300f23f269505ed9
MINWOO LEE <devminwoolee@gmail.com>
2024-11-28 04:43:20 +0000

    feat: add 404 page from PageComponent

--- src/components/pages/ErrorPage.tsx
+++ src/components/pages/ErrorPage.tsx
@@ -1,22 +1,57 @@
 import React from "react";
 import PageComponent from "./PageComponent";
-import { graphql, StaticQuery } from "gatsby";
-import { GatsbyImage } from "gatsby-plugin-image";
 
 import { PageComponentProps } from "../../interfaces";
-import { useThumbnails } from "../hooks/use-thumbnails";
-import { expectedReadTime, formatDate, getBasename } from "../../utils";
+import { Link } from "gatsby";
 
 class ErrorPage extends PageComponent {
-  private property;
-
   constructor(props: PageComponentProps) {
     super(props);
-    this.property = props;
   }
 
   renderContent() {
-    return <></>;
+    return (
+      <center>
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <h1 className="text-4xl font-extrabold text-gray-600">
+          찾을 수 없어요! 🕵️‍♀️
+        </h1>
+        <p className="text-lg">
+          <br />
+          <br />
+          페이지가 사라졌나요? 다른 길을 찾아보세요!
+          <br />
+          <br />
+          <Link
+            to="/"
+            className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+          >
+            메인페이지로
+            <svg
+              className="rtl:rotate-180 w-3.5 h-3.5 ms-2"
+              aria-hidden="true"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 14 10"
+            >
+              <path
+                stroke="currentColor"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                d="M1 5h12m0 0L9 1m4 4L9 9"
+              />
+            </svg>
+          </Link>
+          {/* <Link to="/">메인 페이지로</Link>. */}
+        </p>
+      </center>
+    );
   }
 }
 
--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,50 +1,10 @@
 import * as React from "react";
-import { Link, HeadFC, PageProps } from "gatsby";
-import Header from "../components/common/Header";
-
-const pageStyles = {
-  color: "#232129",
-  padding: "96px",
-  fontFamily: "-apple-system, Roboto, sans-serif, serif",
-};
-const headingStyles = {
-  marginTop: 0,
-  marginBottom: 64,
-  maxWidth: 320,
-};
-
-const paragraphStyles = {
-  marginBottom: 48,
-};
-const codeStyles = {
-  color: "#8A6534",
-  padding: 4,
-  backgroundColor: "#FFF4DB",
-  fontSize: "1.25rem",
-  borderRadius: 4,
-};
+import { HeadFC, PageProps } from "gatsby";
+import ErrorPage from "../components/pages/ErrorPage";
 
 const NotFoundPage: React.FC<PageProps> = () => {
-  return (
-    <main>
-      <Header />
-
-      <h1 style={headingStyles}>Page not found</h1>
-      <p style={paragraphStyles}>
-        Sorry 😔, we couldn’t find what you were looking for.
-        <br />
-        {process.env.NODE_ENV === "development" ? (
-          <>
-            <br />
-            Try creating a page in <code style={codeStyles}>src/pages/</code>.
-            <br />
-          </>
-        ) : null}
-        <br />
-        <Link to="/">Go home</Link>.
-      </p>
-    </main>
-  );
+  const page = new ErrorPage({ id: "-1", title: "404", slug: "404" });
+  return page.render();
 };
 
 export default NotFoundPage;
