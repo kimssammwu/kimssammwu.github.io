682a48cc81c3d00475ee3778347fab837839177d
MINWOO LEE <devminwoolee@gmail.com>
2024-12-26 06:08:25 +0000

    refactor: Edit Image filtering process

--- src/components/hooks/use-thumbnails.tsx
+++ src/components/hooks/use-thumbnails.tsx
@@ -1,10 +1,14 @@
 import { useStaticQuery, graphql } from "gatsby";
-import { useMemo } from "react";
 
 export const useThumbnails = (name: string) => {
   const data = useStaticQuery(graphql`
-    query {
-      allFile(filter: { extension: { regex: "/(png|jpg|jpeg|gif)/" } }) {
+    query getTumbnails($name: String) {
+      allFile(
+        filter: {
+          extension: { regex: "/(png|jpg|jpeg|gif)/" }
+          name: { eq: $name }
+        }
+      ) {
         edges {
           node {
             id
@@ -18,12 +22,8 @@ export const useThumbnails = (name: string) => {
     }
   `);
 
-  const images = useMemo(() => {
-    return data.allFile.edges.reduce((acc, { node }) => {
-      acc[node.name] = node.childImageSharp?.gatsbyImageData;
-      return acc;
-    }, {});
-  }, [data]);
-
-  return images[name];
+  if (data.allFile.edges.length > 0) {
+    return data.allFile.edges[0].node.childImageSharp?.gatsbyImageData;
+  }
+  return null;
 };
