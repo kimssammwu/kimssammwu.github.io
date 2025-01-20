f0135678fe53fed7d344f83fa36e04c287240829
MINWOO LEE <devminwoolee@gmail.com>
2025-01-20 07:28:22 +0000

    feat: Tailwind CSS에 darkMode "selector" 설정 추가  - 사용자 정의 셀렉터로 다크 모드를 적용하기 위해 darkMode: "selector" 설정 추가

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  darkMode: "selector",
   content: [
     `./src/templates/**/*.{js,jsx,ts,tsx}`,
     `./src/pages/**/*.{js,jsx,ts,tsx}`,
